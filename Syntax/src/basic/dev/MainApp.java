e2bb1bd62c326c2e00a37296db40cc8fbc7f5322
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-10-06 08:24:13 +0700

    Add files via upload

---
+++ Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,12 @@
+package basic.dev;
+
+public class MainApp {
+  public static void main(String[] args) {
+	System.out.println("Hello World");
+	int number;
+  }
+	public void show() {
+		
+		
+  }
+}
